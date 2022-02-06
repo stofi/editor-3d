b1ced2c778ec1d9989585ef1147dc0b5daa71cfc
Krystof Rainisch <Krystof.Rainisch@hey.com>
2022-02-06 18:06:44 +0100

    working dual mesh

---
+++ src/Dual.ts
@@ -0,0 +1,147 @@
+import { Vector3 } from 'three'
+
+interface Cell {
+    position: Vector3
+    value: number
+}
+type optionalNumber = number | null
+
+export default class Dual {
+    mainSize: Vector3
+    secondarySize: Vector3
+    main: Cell[] = []
+    secondary: Cell[] = []
+    constructor(size: Vector3) {
+        this.mainSize = size.ceil()
+        this.secondarySize = this.mainSize.clone().addScalar(1)
+        this.main = this.create(this.mainSize)
+        this.secondary = this.create(this.secondarySize)
+    }
+    create(size: Vector3) {
+        const array = new Array(size.x * size.y * size.z)
+            .fill(0)
+            .map((_, index) => {
+                const position = new Vector3()
+                position.x = index % size.x
+                position.y = Math.floor(index / size.x) % size.y
+                position.z = Math.floor(index / size.x / size.y)
+                return {
+                    position,
+                    value: 0,
+                }
+            })
+        return array
+    }
+    positionToIndex(position: Vector3, main = true): optionalNumber {
+        const size = main ? this.mainSize : this.secondarySize
+        if (position.x < 0 || position.x >= size.x) return null
+        if (position.y < 0 || position.y >= size.y) return null
+        if (position.z < 0 || position.z >= size.z) return null
+        return position.x + position.y * size.x + position.z * size.x * size.y
+    }
+    px(position: Vector3, main = true): optionalNumber {
+        const pos = position.clone()
+        pos.x += 1
+        return this.positionToIndex(pos, main)
+    }
+    nx(position: Vector3, main = true): optionalNumber {
+        const pos = position.clone()
+        pos.x -= 1
+        return this.positionToIndex(pos, main)
+    }
+    py(position: Vector3, main = true): optionalNumber {
+        const pos = position.clone()
+        pos.y += 1
+        return this.positionToIndex(pos, main)
+    }
+    ny(position: Vector3, main = true): optionalNumber {
+        const pos = position.clone()
+        pos.y -= 1
+        return this.positionToIndex(pos, main)
+    }
+    pz(position: Vector3, main = true): optionalNumber {
+        const pos = position.clone()
+        pos.z += 1
+        return this.positionToIndex(pos, main)
+    }
+    nz(position: Vector3, main = true): optionalNumber {
+        const pos = position.clone()
+        pos.z -= 1
+        return this.positionToIndex(pos, main)
+    }
+    pxpypz(position: Vector3, main = true): optionalNumber {
+        const pos = position.clone()
+        pos.x += 1
+        pos.y += 1
+        pos.z += 1
+        return this.positionToIndex(pos, main)
+    }
+    nxnz(position: Vector3, main = true): optionalNumber {
+        const pos = position.clone()
+        pos.x -= 1
+        pos.z -= 1
+        return this.positionToIndex(pos, main)
+    }
+    nxpy(position: Vector3, main = true): optionalNumber {
+        const pos = position.clone()
+        pos.x -= 1
+        pos.y += 1
+        return this.positionToIndex(pos, main)
+    }
+    nxpynz(position: Vector3, main = true): optionalNumber {
+        const pos = position.clone()
+        pos.x -= 1
+        pos.y += 1
+        pos.z -= 1
+        return this.positionToIndex(pos, main)
+    }
+    nxny(position: Vector3, main = true): optionalNumber {
+        const pos = position.clone()
+        pos.x -= 1
+        pos.y -= 1
+        return this.positionToIndex(pos, main)
+    }
+    nxnynz(position: Vector3, main = true): optionalNumber {
+        const pos = position.clone()
+        pos.x -= 1
+        pos.y -= 1
+        pos.z -= 1
+        return this.positionToIndex(pos, main)
+    }
+    nynz(position: Vector3): optionalNumber {
+        const pos = position.clone()
+        pos.y -= 1
+        pos.z -= 1
+        return this.positionToIndex(pos)
+    }
+    secondaryToMain(position: Vector3): optionalNumber[] {
+        return [
+            this.nxny(position),
+            this.nxnynz(position),
+            this.nynz(position),
+            this.ny(position),
+            this.nx(position),
+            this.nxnz(position),
+            this.nz(position),
+            this.positionToIndex(position),
+        ]
+    }
+    calculateDual() {
+        this.secondary.forEach((cell) => {
+            this.secondaryToMain(cell.position).forEach(
+                (mainIndex, vertexIndex) => {
+                    if (mainIndex !== null) {
+                        const value = this.main[mainIndex].value
+
+                        cell.value |= value << vertexIndex
+                    }
+                }
+            )
+        })
+    }
+    fillMainWithNoise() {
+        this.main.forEach((cell) => {
+            cell.value = Math.random() > 0.5 ? 1 : 0
+        })
+    }
+}
--- src/Test.ts
+++ src/Test.ts
@@ -1,21 +1,26 @@
 import Scene from './Scene'
 import * as THREE from 'three'
+import Dual from './Dual'
 
 export default class Test extends Scene {
     cubes: THREE.Mesh[] = []
+    duals: THREE.Mesh[] = []
     ambientLight?: THREE.AmbientLight
     directLight?: THREE.DirectionalLight
 
     group = new THREE.Group()
     cubeGeometry = new THREE.BoxBufferGeometry(1, 1, 1)
     cubeMaterial = new THREE.MeshStandardMaterial({
-        color: 0x003300,
-        roughness: 1,
-        // wireframe: true,
-        // vertexColors: true,
+        color: 0x00ff00,
+    })
+    dualMaterial = new THREE.MeshStandardMaterial({
+        color: 0xff33ff,
+        wireframe: true,
     })
     raycaster = new THREE.Raycaster()
     mouse: THREE.Vector2 = new THREE.Vector2()
+    lastTouch = -Infinity
+    dual = new Dual(new THREE.Vector3(3, 3, 3))
 
     constructor(canvas: HTMLCanvasElement) {
         super(canvas)
@@ -26,12 +31,17 @@ export default class Test extends Scene {
         this.addAxisHelper()
     }
 
-    addCube(position?: THREE.Vector3) {
+    addCube(position = new THREE.Vector3(0, 0, 0)) {
+        const mainIndex = this.dual.positionToIndex(position)
+        if (mainIndex === null) return
+
         const cube = new THREE.Mesh(this.cubeGeometry, this.cubeMaterial)
         position && cube.position.copy(position)
         cube.layers.enable(1)
         this.cubes.push(cube)
         this.group.add(cube)
+        this.dual.main[mainIndex].value = 1
+        this.updateDual()
     }
     addAmbientLight() {
         this.ambientLight = new THREE.AmbientLight(0x404040)
@@ -55,7 +65,32 @@ export default class Test extends Scene {
         this.mouse.x = (event.clientX / this.canvas.clientWidth) * 2 - 1
         this.mouse.y = -(event.clientY / this.canvas.clientHeight) * 2 + 1
     }
+    onTouchStart(event: TouchEvent): void {
+        this.lastTouch = event.timeStamp
+    }
+    onTouchEnd(event: TouchEvent): void {
+        const longpress = event.timeStamp - this.lastTouch > 500
+        console.log(longpress, event.timeStamp, this.lastTouch)
+
+        const touch = ((event.changedTouches || [])[0] || {}) as Touch
+
+        this.mouse.x = (touch.clientX / this.canvas.clientWidth) * 2 - 1
+        this.mouse.y = -(touch.clientY / this.canvas.clientHeight) * 2 + 1
+
+        if (longpress) {
+            this.handleRemove()
+        } else {
+            this.handleAdd()
+        }
+    }
     onClick(event: MouseEvent): void {
+        this.handleAdd()
+    }
+    onRightClick(event: MouseEvent): void {
+        event.preventDefault()
+        this.handleRemove()
+    }
+    handleAdd() {
         const intersects = this.getIntersects()
         if (intersects.length > 0) {
             const intersect = intersects[0] as THREE.Intersection
@@ -63,14 +98,56 @@ export default class Test extends Scene {
             // index of intersected object
             const position = object.position.clone()
             const normal = intersect?.face?.normal.clone() ?? null
-            normal && this.addCube(position.add(normal))
+            if (!normal) return
+            position.add(normal)
+            this.addCube(position)
         }
     }
+    handleRemove() {
+        const intersects = this.getIntersects()
+        if (intersects.length > 0) {
+            const intersect = intersects[0] as THREE.Intersection
+            const object = intersect.object as THREE.Mesh
+            const index = this.cubes.indexOf(object)
 
+            const mainIndex = this.dual.positionToIndex(object.position)
+            if (mainIndex === null) return
+            if (index > -1) {
+                this.dual.main[mainIndex].value = 0
+                this.cubes.splice(index, 1)
+                this.group.remove(object)
+            }
+        }
+        this.updateDual()
+    }
+    addDualCube(position?: THREE.Vector3) {
+        const dualCube = new THREE.Mesh(this.cubeGeometry, this.dualMaterial)
+        position && dualCube.position.copy(position).subScalar(0.5)
+        this.duals.push(dualCube)
+        this.group.add(dualCube)
+    }
+    updateDual(): void {
+        this.dual.calculateDual()
+        for (let i = this.duals.length - 1; i >= 0; i--) {
+            const dualCube = this.duals[i]
+            this.group.remove(dualCube)
+            this.duals.splice(i, 1)
+        }
+
+        this.dual.secondary.forEach((dual, index) => {
+            if (dual.value === 0) return
+            const position = dual.position
+            this.addDualCube(position)
+        })
+    }
     addListeners(): void {
         super.addListeners()
         window.addEventListener('mousemove', this.onMouseMove.bind(this))
         window.addEventListener('click', this.onClick.bind(this))
+        //capture right click
+        window.addEventListener('contextmenu', this.onRightClick.bind(this))
+        window.addEventListener('touchend', this.onTouchEnd.bind(this))
+        window.addEventListener('touchstart', this.onTouchStart.bind(this))
     }
     tick(): void {
         super.tick()
